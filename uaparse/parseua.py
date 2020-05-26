06fea402484f36ec6395e9c9c744646991b4bd16
pankajthekush <pankaj.kushwaha@rho.ai>
2020-05-26 11:10:56 +0530

    print statement

--- uaparse/parseua.py
+++ uaparse/parseua.py
@@ -62,6 +62,7 @@ def insert_to_db(in_file_name = 'outcsv.csv'):
             
             if index % 1000 == 0:
                 session.commit()
+                print('commited')
                 
         
     session.commit()
