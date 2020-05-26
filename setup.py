db8016314eafc57794c73f967c39804eb7c5d4ef
pankajthekush <pankaj.kushwaha@rho.ai>
2020-05-26 11:04:09 +0530

    commit on 1000

--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ current_path  = os.path.dirname(os.path.realpath(__file__))
 
 setup(
     name='uaparse',
-    version='1.2',
+    version='1.3',
     packages=['uaparse'],
     install_requires=['SQLAlchemy>=1.3.16','beautifulsoup4>=4.9.0','ua-parser>=0.10.0'],
     entry_points ={'console_scripts': ['parseupload = uaparse.parseua:parse_upload',
-                                        'parseonly = uaparse.parseua:ua_from_html'        ]} 
+                                        'parseonly = uaparse.parseua:ua_from_html',
+                                        'uploadonly=uaparse.parseua:insert_to_db']} 
 )
--- uaparse/parseua.py
+++ uaparse/parseua.py
@@ -63,7 +63,7 @@ def insert_to_db(in_file_name = 'outcsv.csv'):
             if index % 1000 == 0:
                 session.commit()
                 
-          
+        
     session.commit()
     session.close()
 
